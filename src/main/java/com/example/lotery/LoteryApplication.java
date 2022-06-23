fae16f58569238b41e957a3646772c22ef1ec541
luma <luma2906@gmail.com>
2022-06-22 22:08:06 -0500

    first commit

---
+++ src/main/java/com/example/lotery/LoteryApplication.java
@@ -0,0 +1,13 @@
+package com.example.lotery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteryApplication.class, args);
+	}
+
+}
